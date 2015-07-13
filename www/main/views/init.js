ed242504b64d356c0b8a7d2ff3cd2537491e60a9
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2015-07-13 14:49:34 +0200

    feat(init)

--- www/main/views/init.js
+++ www/main/views/init.js
@@ -18,18 +18,18 @@
   }
 
 
-  function InitController(dataService, $state, $ionicHistory) {
+  function InitController(dataService, $state, $ionicHistory, $timeout) {
     var vm = this; // view-model
-    //dataService.loadCsv()
-    //  .then(function () {
-    //    $ionicHistory.nextViewOptions({
-    //      disableAnimate: true,
-    //      disableBack:    true
-    //    });
-    //    $state.go('welcome');
-    //  }).error(function (err) {
-    //    alert(err);
-    //  });
+    dataService.loadCsv()
+      .then(function () {
+        $timeout(function () {
+          $ionicHistory.nextViewOptions({
+            //disableAnimate: true,
+            disableBack:    true
+          });
+          $state.go('welcome');
+        }, 1000);
+      });
   }
 
 
