9c819f4732fa46c1c573ad5348f3ceb700b5f732
Roman <roman.dyck@students.fhnw.ch>
2015-08-31 15:41:19 +0200

    Fixed language on Initial start

--- www/main/views/tutorial.js
+++ www/main/views/tutorial.js
@@ -22,22 +22,14 @@
 
   function TutorialController($translate, $rootScope) {
     var vm = this; // view-model
-    var sysLang = navigator.language.substring(0,2);
-    console.log(sysLang);
-
-    if((sysLang=="de" || sysLang=="en" || sysLang=="fr")){
-      vm.language=angular.uppercase(sysLang);
-      $rootScope.$broadcast('onLanguageChange', vm.language.toLowerCase());
-    }else{
-      vm.language= angular.uppercase($translate.use());
-    }
+    vm.language=angular.uppercase($rootScope.settings.language);
+    $rootScope.$broadcast('onLanguageChange', vm.language.toLowerCase());
 
     $rootScope.$on('onLanguageChange', function(event, langKey){
         vm.language = angular.uppercase(langKey);
     });
 
     vm.onLanguageChange = function () {
-      //$translate.use(vm.language.toLowerCase());
       $rootScope.$broadcast('onLanguageChange', vm.language.toLowerCase());
     };
   }
