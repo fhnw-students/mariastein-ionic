93e0afab3624036e910566bcb840e843f1960890
David Heimgartner <david.heimgartner@students.fhnw.ch>
2015-09-28 20:24:07 +0200

    feat(News): init js feat(NewsPage) init js

--- www/main/views/news.js
+++ www/main/views/news.js
@@ -4,33 +4,71 @@
   var namespace = 'kmsscan.views.News';
 
   angular.module(namespace, [
-    'kmsscan.utils.Logger'
+    'kmsscan.utils.Logger',
+    'kmsscan.services.stores.Pages',
+    'kmsscan.services.stores.Settings'
   ])
-    .config(StateConfig)
-    .controller('NewsCtrl', NewsController);
-
+      .config(StateConfig)
+      .controller('NewsCtrl', NewsController);
 
   function StateConfig($stateProvider) {
     $stateProvider
-      .state('menu.news', {
-        url: '/news',
-        views: {
-          'menuContent': {
-            templateUrl: 'main/views/news.html',
-            controller: 'NewsCtrl as news'
+        .state('menu.news', {
+          url: '/news',
+          views: {
+            'menuContent': {
+              templateUrl: 'main/views/news.html',
+              controller: 'NewsCtrl as news'
+            }
           }
-        }
-      });
+        });
   }
 
-
-  function NewsController(Logger) {
-    var vm = this;  // view-model
+  function NewsController($q, $scope, $rootScope, Logger, pagesStoreService, settingsStoreService) {
     var log = new Logger(namespace);
+    var vm = this; // view-model
+    vm.isPending = true;
+    vm.hasFailed = false;
+    vm.pages = [];
 
+    vm.isReady = isReady;
 
+    if ($rootScope.syncIsActive) {
+      $rootScope.$on('kmsscan.run.activate.succeed', activate);
+    } else {
+      activate();
+    }
+
+    var eventIndexOnChange = settingsStoreService.onChange(function () {
+      activate();
+    });
+
+    $scope.$on('$destroy', function () {
+      settingsStoreService.offChange(eventIndexOnChange);
+    });
+    /////////////////////////////
+    function activate() {
+      settingsStoreService.get()
+          .then(function (settings) {
+            return pagesStoreService.getVisited(settings.language);
+          })
+          .then(function (pages) {
+            log.debug('activate() -> succeed', pages);
+            vm.pages = pages;
+            vm.isPending = false;
+            vm.hasFailed = false;
+          })
+          .catch(function (err) {
+            log.error('Failed to load visited pages!', err);
+            vm.isPending = false;
+            vm.hasFailed = true;
+          });
+    }
+
+    function isReady() {
+      return !$rootScope.syncIsActive && !vm.isPending;
+    }
 
   }
-
 
 }());
--- www/main/views/newsPage.js
+++ www/main/views/newsPage.js
@@ -1,32 +1,141 @@
 (function () {
   'use strict';
 
-  var namespace = 'kmsscan.views.NewsPage';
+  var namespace = 'kmsscan.views.News';
 
   angular.module(namespace, [
-    'kmsscan.utils.Logger'
+    'kmsscan.utils.Logger',
+    'kmsscan.services.stores.News',
+    'kmsscan.services.stores.Settings'
   ])
-    .config(StateConfig)
-    .controller('NewsPageCtrl', NewsPageController);
+      .config(StateConfig)
+      .controller('NewsPageCtrl', NewsPageController);
 
   function StateConfig($stateProvider) {
     $stateProvider
-      .state('menu.newsPage', {
-        url: '/newsPage/:id',
-        views: {
-          'menuContent': {
-            templateUrl: 'main/views/newsPage.html',
-            controller: 'NewsPageCtrl as newsPage'
+        .state('menu.newsPage', {
+          url: '/newsPage/:uid',
+          views: {
+            'menuContent': {
+              templateUrl: 'main/views/newsPage.html',
+              controller: 'NewsPageCtrl as newsPage'
+            }
           }
-        }
 
-      });
+        });
   }
 
-  function NewsPageController($stateParams, Logger) {
+  function NewsPageController($window, $stateParams, $ionicModal, $ionicSlideBoxDelegate, $ionicScrollDelegate,
+                            $rootScope, Logger, newsPageStoreService, settingsStoreService, $scope) {
     var vm = this; // view-model
     var log = new Logger(namespace);
+    vm.doc = {};
+    vm.settings = {};
+    vm.isPending = true;
+    vm.hasFailed = false;
+    vm.more = false;
+    vm.zoomMin = 1;
+    vm.enableZoom = false;
+    vm.hgt = $window.innerHeight - 50;
+    vm.hgt2 = $window.innerHeight - 145;
 
+    vm.isReady = isReady;
+    vm.showImages = showImages;
+    vm.showModal = showModal;
+    vm.closeModal = closeModal;
+    vm.scrollTop = scrollTop;
+    vm.zoom = zoom;
+    vm.updateSlideStatus = updateSlideStatus;
+
+    if ($rootScope.syncIsActive) {
+      $rootScope.$on('kmsscan.run.activate.succeed', activate);
+    } else {
+      activate();
+    }
+
+    var eventIndexOnChange = settingsStoreService.onChange(function () {
+      activate();
+    });
+
+    $scope.$on('$destroy', function () {
+      settingsStoreService.offChange(eventIndexOnChange);
+    });
+    /////////////////////////////
+    function activate() {
+      settingsStoreService.get()
+          .then(function (settings) {
+            vm.settings = settings;
+            return newsStoreService.get($stateParams.uid, settings.language);
+          })
+          .then(function (doc) {
+            log.debug('activate() -> succeed', doc);
+            vm.doc = doc;
+            vm.isPending = false;
+            vm.hasFailed = false;
+          })
+          .catch(function (err) {
+            log.error('Failed to load doc!', err);
+            vm.isPending = false;
+            vm.hasFailed = true;
+          });
+    }
+
+    function isReady() {
+      return !$rootScope.syncIsActive && !vm.isPending;
+    }
+
+
+    function showImages(index) {
+      vm.activeSlide = index;
+      vm.showModal('main/views/modalPreview.html');
+    }
+
+    function showModal(templateUrl) {
+      $ionicModal.fromTemplateUrl(templateUrl, {
+        scope: $scope
+      })
+          .then(function (modal) {
+            vm.modal = modal;
+            vm.modal.show();
+          });
+    }
+
+    function closeModal() {
+      vm.modal.hide();
+      vm.modal.remove();
+    }
+
+    function scrollTop() {
+      if (vm.settings.zooming) {
+        vm.enableZoom = true;
+        $ionicScrollDelegate.$getByHandle('scrollMain')
+            .scrollTo(0, 200, true);
+      }
+    }
+
+    function updateSlideStatus(slide) {
+      var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + slide)
+          .getScrollPosition()
+          .zoom;
+      if (zoomFactor === vm.zoomMin) {
+        $ionicSlideBoxDelegate.enableSlide(true);
+      } else {
+        $ionicSlideBoxDelegate.enableSlide(false);
+      }
+    }
+
+    function zoom(slide) {
+      var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + slide)
+          .getScrollPosition()
+          .zoom;
+      if (zoomFactor === vm.zoomMin) {
+        $ionicScrollDelegate.$getByHandle('scrollHandle' + slide)
+            .zoomBy(2, true);
+      } else {
+        $ionicScrollDelegate.$getByHandle('scrollHandle' + slide)
+            .zoomTo(1, true);
+      }
+    }
 
   }
 
