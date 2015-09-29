c63916fce638ba9347f8fdee27dd4b0b787e9c5e
Gery Hirschfeld <hirsch1988@gmail.com>
2015-09-29 14:13:14 +0200

    refactor(not-found-view)

--- www/main/views/notFound.js
+++ www/main/views/notFound.js
@@ -1,3 +1,11 @@
+/**
+ * @module kmsscan.views.NotFound
+ * @author Gerhard Hirschfeld
+ *
+ * @description
+ * This view shows a static view for not found objects
+ *
+ */
 (function () {
   'use strict';
 
