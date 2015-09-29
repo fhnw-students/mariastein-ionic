5467916130a79aa3c10843b9167d7dbeb3f26fd7
Luecu <luca.indermuehle@gmail.com>
2015-09-29 10:50:41 +0200

    update test config

--- karma-unit.config.js
+++ karma-unit.config.js
@@ -36,38 +36,66 @@ module.exports = function (config) {
 
   // angular script files & 3rd party code from bower
   conf.files = conf.files.concat([
-    'www/lib/ionic/js/ionic.bundle.js',
-    'www/lib/lodash/lodash.min.js',
-    'www/lib/angular-sanitize/angular-sanitize.js',
-    'www/lib/angular-translate/angular-translate.min.js',
-    'www/lib/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
-    'www/lib/papaparse/papaparse.min.js',
-    'www/lib/localforage/dist/localforage.min.js',
-    'www/lib/angular-localForage/dist/angular-localForage.min.js',
+      'www/lib/ionic/js/ionic.bundle.js',
+      'www/lib/lodash/lodash.min.js',
+      'www/lib/angular-sanitize/angular-sanitize.js',
+      'www/lib/angular-translate/angular-translate.min.js',
+      'www/lib/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
+      'www/lib/moment/min/moment.min.js',
+      'www/lib/pouchdb/dist/pouchdb.min.js',
+      'www/lib/pouchdb-find/dist/pouchdb.find.min.js',
+      'www/lib/angular-pouchdb/angular-pouchdb.min.js',
 
-    'www/main/app.js',
-    'www/main/services/services.module.js',
-    'www/main/directives/directives.module.js',
-    'www/main/views/views.module.js',
-    'www/main/core/ionicPlatform.js',
-    'www/main/core/routerConfig.js',
-    'www/main/core/translateConfig.js',
-    'www/main/services/barcodeScanner.service.js',
-    'www/main/services/data.service.js',
-    'www/main/services/history.service.js',
-    'www/main/directives/navMenuButton.js',
-    'www/main/directives/scanButton.js',
-    'www/main/app.run.js',
-    'www/main/views/init.js',
-    'www/main/views/tutorial.js',
-    'www/main/views/detail.js',
-    'www/main/views/notFound.js',
-    'www/main/views/welcome.js',
-    'www/main/views/contact.js',
-    'www/main/views/history.js',
-    'www/main/views/news.js',
-    'www/main/views/menu.js',
-    'www/main/views/scan.js'
+        <!-- cordova script (this will be a 404 during development) -->
+      'www/lib/ngCordova/dist/ng-cordova.min.js',
+      'www/cordova.js',
+
+        <!-- your app's js -->
+      'www/main/app.js',
+
+      'www/main/services/services.module.js',
+      'www/main/services/rest/rest.module.js',
+      'www/main/services/stores/stores.module.js',
+
+      'www/main/directives/directives.module.js',
+      'www/main/views/views.module.js',
+
+      'www/main/utils/utils.module.js',
+      'www/main/utils/logger.js',
+      'www/main/utils/helpers.js',
+      'www/main/utils/pouchDb.js',
+
+      'www/main/core/ionicPlatform.js',
+      'www/main/core/routerConfig.js',
+      'www/main/core/translateConfig.js',
+
+      'www/main/services/rest/typo3.service.js',
+      'www/main/services/stores/settingsStore.service.js',
+      'www/main/services/stores/pagesStore.service.js',
+      'www/main/services/stores/roomsStore.service.js',
+      'www/main/services/images.service.js',
+
+      'www/main/directives/navMenuButton.js',
+      'www/main/directives/scanButton.js',
+      'www/main/directives/spinner.js',
+      'www/main/directives/image.js',
+
+      'www/main/app.run.js',
+
+      'www/main/views/init.js',
+      'www/main/views/tutorial.js',
+      'www/main/views/detail.js',
+      'www/main/views/notFound.js',
+      'www/main/views/welcome.js',
+      'www/main/views/contact.js',
+      'www/main/views/history.js',
+      'www/main/views/news.js',
+      'www/main/views/newsPage.js',
+      'www/main/views/map.js',
+      'www/main/views/mapPage.js',
+      'www/main/views/menu.js',
+      'www/main/views/scan.js',
+      'www/main/views/settings.js'
   ]);
 
   // All Templates
