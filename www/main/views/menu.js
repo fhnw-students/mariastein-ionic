1f975c831edd151a0caf6ef32d1c304738ac18b5
Gery Hirschfeld <hirsch1988@gmail.com>
2015-09-29 14:09:29 +0200

    refactor(menu)

--- www/main/views/menu.js
+++ www/main/views/menu.js
@@ -1,56 +1,25 @@
+/**
+ * @module kmsscan.views.Menu
+ * @author Gianni Alagna
+ *
+ * @description
+ * This view shows the abstract view with the menu
+ *
+ */
 (function () {
   'use strict';
 
   var namespace = 'kmsscan.views.Menu';
 
-  angular.module(namespace, [
-    'kmsscan.utils.Logger',
-    'kmsscan.services.stores.Settings'
-  ])
-    .config(StateConfig)
-    .controller('MenuCtrl', MenuController);
+  angular.module(namespace, [])
+    .config(StateConfig);
 
   function StateConfig($stateProvider) {
     $stateProvider.state('menu', {
       url: '/menu',
       abstract: true,
-      templateUrl: 'main/views/menu.html',
-      controller: 'MenuCtrl as menu'
+      templateUrl: 'main/views/menu.html'
     });
   }
 
-  function MenuController(Logger, $rootScope, settingsStoreService) {
-    var vm = this;
-    var log = new Logger(namespace);
-
-    vm.settings = {};
-    vm.saveSettings = saveSettings;
-    vm.onLanguageChange = onLanguageChange;
-
-    activate();
-    ///////////////////////////////
-    function activate() {
-      $rootScope.$on('onLanguageChange', function (event, langKey) {
-        vm.settings.language = angular.uppercase(langKey);
-      });
-      settingsStoreService.get().then(function (settings) {
-        log.debug('activate() - success', settings);
-        vm.settings = settings;
-      });
-    }
-
-    function saveSettings() {
-      return settingsStoreService.set(vm.settings).then(function (settings) {
-        log.debug('saveSettings() - success', settings);
-        vm.settings = settings;
-        return settings;
-      });
-    }
-
-    function onLanguageChange() {
-      saveSettings(vm.settings).then(function () {
-        $rootScope.$broadcast('onLanguageChange', vm.settings.language);
-      });
-    }
-  }
 }());
